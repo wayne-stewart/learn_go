55cb1d2855598c07f66990b87047ffea45d3a2b2
Wayne Stewart <waynegaidin@hotmail.com>
2022-05-03 00:35:18 -0500

    finished part 2 of excersize 1 game quiz

--- gophersize/01_quiz_game/main.go
+++ gophersize/01_quiz_game/main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csv_file_name := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	time_limit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csv_file_name)
@@ -25,13 +27,25 @@ func main() {
 
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*time_limit) * time.Second)
 	correct := 0
+
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		a_ch := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			a_ch <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-a_ch:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
